2ef8d91876f5f48a0fc03b227bac60856f6b86aa
ioah86 <albert.heinle@googlemail.com>
2015-02-14 16:18:29 -0500

    [INITIAL]
    Initial Commit. Ready version of repetition_error.el posted.

---
+++ repetition-error.el
@@ -0,0 +1,404 @@
+;;; repetition-error.el --- Interactive tools to find repetition errors in the buffer.
+
+;; Copyright (C) 2015  Albert Heinle
+
+;; Author: Albert Heinle <albert.heinle@googlemail.com>
+;; Keywords: matching, convenience, files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General Description                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; The main function in this package is "find-repetition-error". It
+;; takes a starting point and an end point, and if the section is long
+;; enough (i.e. enough words given), it will highlight repetitions,
+;; and ask the user either to continue and ignore that repetition, or
+;; to quit the search. In the latter case, the current cursor remains
+;; at the position, and the user can correct the error, if he or she
+;; wants.
+;; There are the following functions that can be interactively
+;; called.
+;; find-repetition-error-whole-buffer:
+;;   Runs the function "find-repetition-error" from the beginning
+;;   until the end of the whole buffer
+;; find-repetition-error-from-point:
+;;   Runs the function "find-repetition error" from the current cursor
+;;   position until the end of the document. This can be also used as
+;;   a way to resume a previously stopped repetition error search
+;;   (after the error has been corrected -- or not)
+;;
+;; Just try to call one of these functions, and the usage is quite
+;; straight forward.
+;;
+;; What do our functions recognize as words:
+;;   - Anything fulfilling the following regular expression:
+;;     [a-zA-Z]{4,}, i.e. any trailing whitespaces or other symbols
+;;     will be ignored.
+;; How does the function move forward?
+;;   - We always check a block of size 100 (default value, which can
+;;     be changed by setting the variable
+;;     repetition-error-word-block-size). If nothing is found in this
+;;     block, the starting point moves forward by one word and tries
+;;     again. This is repeated until either the last 100 words are reached
+;;     and nothing was found, or until a repetition error has been
+;;     revealed. If the user decides to ignore it and moves forward, the
+;;     repeated word in that block will be saved with its position. In the
+;;     moment, when the cursor moves forward, all ignored repeated words
+;;     will be saved with the information about the block they have been
+;;     encountered in. This is done because we don't want to repeatedly
+;;     warn the user about repeated words in the same area, and these
+;;     word will be ignored for all areas that intersect with the already
+;;     considered and ignored one.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; TODOs:                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   - LaTeX support (i.e. ignore LaTeX keywords)
+;;   - Experiment with different block sizes and repetition
+;;     occurrences.
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CHANGELOG:                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   - Mi 14. Jan 22:30:15 EST 2015:
+;;      Initial beta version done.
+;;   - Mo 2. Feb 11:04:53 EST 2015:
+;;      - Added function to find repetition errors starting from current
+;;        current cursor position.
+;;      - removed save-recursion from find-repetition error function.
+;;   - Fr 6. Feb 23:23:57 EST 2015:
+;;      - Altered the documentation a bit
+;;      - General description given of the main functionality.
+
+
+;;; Code:
+
+
+(defvar repetition-error-word-block-size 100
+"
+ (Positive Integer)
+This variable determines the block size (i.e. the number of words)
+our repetition-error-finding-routines will consider when trying to
+find repetition errors. The initial value is 100
+"
+);repetition-error-word-block-size
+
+(defvar repetition-error-min-occurrence 2
+"
+ (Positive Integer)
+This variable determines the minimum number of repetitions for a certain
+word to appear in a word-block to be considered a repetition error.
+The initial value is 2.
+"
+);repetition-error-min-occurrence
+
+(defun transform-complete-ci-string (s)
+"string->string
+This function consumes a string s, and replaces any letter in this
+string to the regular expression accepting both the upper-case, as
+well as the lower-case of that same letter. For example, the string
+'ab' would be transformed into '[aA][bB]'.
+"
+  (replace-regexp-in-string "[[:alpha:]]"
+	 (lambda (z) (concat "[" (downcase z) (upcase z) "]"))
+	 s)
+);transform-complete-ci-string
+
+(defun find-repetition-error-whole-buffer ()
+"None->None
+This function will scan the whole buffer for repetitions of certain
+words.  If the buffer does not have repetition-error-word-block-size
+words (default: 100) then nothing is returned.  If in between, there
+is a block found with repetition-error-word-block-size words and a
+repetition of repetition-error-min-occurrence, then the repeated words
+will be highlighted for the user. Then he or she can decide, if s/he
+wants to go to the next repetition error or not using a
+command-prompt.
+SIDE-EFFECT:
+ - Takes user inputs
+ - Highlights text
+"
+  (interactive)
+  (find-repetition-error (point-min) (point-max) repetition-error-word-block-size repetition-error-min-occurrence)
+);find-repetition-error-whole-buffer
+
+(defun find-repetition-error-from-point ()
+"None->None
+This function will scan the whole buffer, starting from the current
+cursor position, for repetitions of certain
+words.  If the buffer does not have repetition-error-word-block-size
+words (default: 100) then nothing is returned.  If in between, there
+is a block found with repetition-error-word-block-size words and a
+repetition of repetition-error-min-occurrence, then the repeated words
+will be highlighted for the user. Then he or she can decide, if s/he
+wants to go to the next repetition error or not using a
+command-prompt.
+SIDE-EFFECT:
+ - Takes user inputs
+ - Highlights text
+"
+  (interactive)
+  (find-repetition-error (point) (point-max) repetition-error-word-block-size repetition-error-min-occurrence)
+);find-repetition-error-whole-buffer
+
+
+(defun find-repetition-error (begin end &optional nWords minRep)
+"integer->integer(->integer->integer)->None
+This function will scan the buffer between the character at position
+begin and the character at position end for repetitions of certain
+words. Begin and end are non-negative integers. Optionally, the user
+can also define two integers nWords and minRep, which will make the
+function return a finding, if there are more or equal minRep words
+repeated in a block of nWords words. The default value for nWords is
+100, the default value for minRep is 2, represented by the global
+variables repetition-error-word-block-size and
+repetition-error-min-occurrence.  If the block does not have nWords
+words, then nothing is returned.  If in between, there is a block
+found with nWords words and a repetition of minRep, then the repeated
+words will be highlighted for the user. Then he or she can decide, if
+s/he wants to go to the next repetition error or not using a
+command-prompt.
+SIDE EFFECTS:
+ - Takes user input
+ - Highlights text
+"
+  (if (not nWords)
+      (setq nWords repetition-error-word-block-size);then
+  );if
+  (if (not minRep)
+      (setq minRep repetition-error-min-occurrence);then
+  );if
+  ;(save-excursion
+  (if (<= end begin)
+      "Invalid bounds"
+    (goto-char begin)
+    (recenter 0)
+    (let 
+	(;let definitions
+	 (flag t)
+	 (curWordBlock nil)
+	 (exc nil)
+	 (tempExc nil)
+	 (tempKnownsList nil)
+	 (usrcmd nil)
+	);let definitions
+      (while flag
+	(setq curWordBlock (get-next-n-words-from-point nWords (point)))
+	(if
+	  (equal curWordBlock "")
+	  (progn
+	    (setq flag nil)
+	    "Reached the end of the buffer"
+	  );progn for then
+		;else
+	  (setq exc (filter-known-words tempKnownsList (exceeders curWordBlock minRep) (point) (+ (point) (string-width curWordBlock))))
+	  (setq tempKnownsList (update-knowns-list tempKnownsList exc (point) (+ (point) (string-width curWordBlock))))
+	  (if (equal exc ())
+	      (progn
+		(re-search-forward "[[:space:]\n]" end t)
+		(recenter 0)
+		(if (> (point) end)
+		  (setq flag nil)
+		);if
+	      );progn
+		;else
+	    (setq tempExc exc)
+	    (while (not (equal tempExc ()))
+	      (highlight-regexp (transform-complete-ci-string (car (car tempExc))))
+	      (setq usrcmd 
+		    (read-char (format "Repeated word: \"%s\". (c) Continue search for repetition errors or (any key) quit?" (car (car tempExc))))
+	      );setq
+	      (unhighlight-regexp (transform-complete-ci-string (car (car tempExc))))
+	      (setq tempExc (cdr tempExc))
+	      (if (not (equal usrcmd 99));;99 is ASCII for 'c'
+                (progn
+		  (setq flag nil)
+		  (setq tempExc ())
+		);progn
+	      );if
+	    );while
+	    (re-search-forward "[[:space:]\n]" end t)
+	    (recenter 0)
+	    (if (> (point) end)
+	      (setq flag nil)
+	    );if
+	  );if
+	);if
+      );while
+    );let
+  );if
+  (message "Finished finding repetition errors")
+  ;);save-excursion
+);find-repetition-error
+
+(defun update-knowns-list (knownList newExceeders leftBound
+				     rightBound)
+"(listof (list string int int))->(listof (list string int))->int->int->(listof (list string int))
+This function consumes a list, knownlist, whose entries are tuples of a string and two integers,
+a list with tuples of string and int, newExceeders, and two integers, leftBound and rightBound.
+It first filters all the elements (s i j) in knownslist out, where j<leftbound. Then 
+it returns a concatenation of the filtered knownList and a list containing for every (s i) in newExceeders a tuple
+ (s leftBound rightbound).
+ASSUMPTIONS:
+ - newExceeders has no intersection with knownslist, given the left and the right bound.
+"
+  (let
+      (;let definitions
+       (tempNE newExceeders)
+       (result (remove-if (lambda (m) (if (< (third m) leftBound) t nil)) knownList))
+      );let definitions
+    (while (not (equal tempNE ()))
+      (setq result 
+	    (cons (cons (first (first tempNE)) (cons leftBound (cons rightBound())))
+		  result))
+      (setq tempNE (rest tempNE))
+    );while
+    result
+  );let
+);update-knowns-list
+
+(defun filter-known-words (knownList newExceeders leftBound rightBound)
+"(listof (list string int int))->(listof (list string int))->int->int->(listof (list string int))
+This function consumes a list, knownlist, whose entries are tuples of a string and two integers,
+a list with tuples of string and int, newExceeders, and two integers, leftBound and rightBound.
+It returns a filtered copy of the list newExceeders: If there is an entry (s i j) in knownList,
+and the intervals [i,j] nad [leftBound, rightBound] have a nontrivial intersection, it will be omitted.
+ASSUMPTIONS:
+ - In every entry (s i j) of knownList, we always have i<j
+ - leftBound < rightBound
+"
+  (labels
+      (;labels definitions
+       (compareToKnownList (kl el)
+	  "This helper function consumes two lists, kl and el. kl
+consists of 3 tuples, consisting of a string and two integers. el is a
+list which first element is a string. This function returns true, if
+the string in el does coincide with the string in at least one of the
+elements in kl, and when this element's second and third element, say
+ (u,v), has an intersection with (leftBound, rightBound) when viewing
+them as intervals."
+	  (let
+	    (;let definitions
+	       (tempKL kl)
+	       (flag nil)
+	    );let definitions
+	    (while (and (not flag) (not (equal tempKL ())))
+	      (if
+	        (and
+		   (equal (first (first tempKL)) (first el))
+		   (< (second (first tempKL)) rightBound)
+		   (> (third (first tempKL)) leftBound)
+	        );and
+		(setq flag t)
+	      );if
+	      (setq tempKL (rest tempKL))
+	    );while
+	    flag
+	  );let
+       );compareToKnownList
+      );labels definitions
+    (remove-if (lambda (el) (compareToKnownList knownList el)) newExceeders)
+  );labels
+);filter-known-words
+
+;(filter-known-words '(("abc" 1 20) ("cde" 5 25)) '(("abc" 4) ("def" 2)) 19 30)
+;;(filter-known-words '() '(("abc" 4) ("def" 2)) 19 30)
+;; (first '(("abc" 1 20)))
+
+
+(defun get-next-n-words-from-point (n p)
+"Integer->Integer->string
+Given an integer n and an integer p. The parameter p represents a
+position in the buffer, n represents a number of words we want to
+extract. This function returns a string containing the next n words
+from point p in the buffer, if available. If there are no n words,
+then the function returns the empty string.
+ASSUMPTIONS:
+ - The point n is at the beginning of a word
+SIDE EFFECTS:
+ - The cursor will in the end actually be moved to position p
+ - Accesses cursor positions
+"
+  (goto-char p)
+  (let
+    (;let definitions
+     (flag t)
+     (i n)
+     (curpos (point))
+    );let definitions
+    (while (and (> i 0) flag)
+      (setq curpos (point))
+      (re-search-forward "[[:space:]\n]" (point-max) t)
+      (if (equal curpos (point))
+	  (setq flag nil)
+      );if
+      (setq i (- i 1))
+    );while
+    (goto-char p)
+    (if flag
+	(buffer-substring-no-properties p curpos)
+        ""
+    );if
+  );let
+);;get-next-n-words-from-point
+
+(defun exceeders (str number)
+"string->integer->listof (list string integer)
+Given a string str, and a number 'number', this function returns a
+list of tuples (a, b), where a is a word in str, which appears b
+times in str, where b>=number. Furthermore, for a, we ignore it
+if its length is smaller than 4 letters, and we remove all space
+characters, as well as digits and punctuation symbols.
+"
+  (labels 
+      (;function definitions
+       (count-each-word (l)
+	 (let
+	     (;let definitions
+	      (tempList l)
+	      (occ 0)
+	      (result ())
+	     );let definitions
+	   (while (not (equal tempList ()))
+	     (setq occ (count-if (lambda (m) (equal (downcase m) (downcase (car tempList)))) tempList))
+	     (setq result (cons (cons (downcase (car tempList)) (cons occ ()))
+				result))
+	     (setq tempList (remove-if (lambda (m) (equal (downcase m) (downcase (car tempList)))) tempList))
+	   );while
+	   (nreverse result)
+	 );let
+	);count-each-word
+       );;function definitions
+    (;labels body
+     let
+      (;let definitions
+       (filtered-inp (remove-if (lambda (m) (< (string-width m) 4))
+				(split-string str
+					      "[[:punct:][:digit:][:space:]\n]"
+					      t)))
+      );;let definitions
+      (;let body
+       remove-if (lambda (k) (< (second k) number)) (count-each-word
+						     filtered-inp)
+      );let body
+    );labels body
+  );;labels
+);exceeders
+
+(provide 'repetition-error)
+;;; repetition-error.el ends here
